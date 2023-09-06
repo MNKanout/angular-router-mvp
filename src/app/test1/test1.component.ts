24770b40e0e2c01fbc312e6de15aacca632255cf
Nour <mnkanout@gmail.com>
2023-09-06 15:52:43 +0200

    ng generate component test1 --skip-tests --inline-template

---
+++ src/app/test1/test1.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test1',
+  template: `
+    <p>
+      test1 works!
+    </p>
+  `,
+  styleUrls: ['./test1.component.css']
+})
+export class Test1Component {
+
+}
