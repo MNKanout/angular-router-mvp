f7e6653b746e0ca0f4527bdb0a03b33997e951e3
Nour <mnkanout@gmail.com>
2023-09-06 16:20:23 +0200

    Added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { provideRouter } from '@angular/router';
+import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { Test1Component } from './test1/test1.component';
 import { Test2Component } from './test2/test2.component';
+
+const routes: Routes =[
+  {
+    path:'test1',
+    component: Test1Component,
+    title: 'test1',
+  },
+  {
+    path:'test2',
+    component: Test2Component,
+    title: 'test2',
+  },
+]
 
 @NgModule({
   declarations: [
@@ -16,7 +31,9 @@ import { Test2Component } from './test2/test2.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    provideRouter(routes),
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
